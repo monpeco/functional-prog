becfa32a0f46d55f8c8808e8cc732cf933061123
monpeco <monpeco@gmail.com>
2017-02-14 12:24:36 +0000

    Update sum-seq, add comment and use of the funtion

--- sum-sequence-a1.py
+++ sum-sequence-a1.py
@@ -1,4 +1,14 @@
+"""
+We've defined the sum of a sequence in two cases: the base case states that the
+sum of a zero length sequence is 0, while the recursive case states that the sum
+of a sequence is the first value plus the sum of the rest of the sequence. Since the
+recursive definition depends on a shorter sequence, we can be sure that it will
+(eventually) devolve to the base case.
+"""
 def sum(seq):
     if len(seq) == 0: return 0
     return seq[0] + sum(seq[1:])
 
+
+s=sum(range(5))
+print(s)
