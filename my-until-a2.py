2abb587222b9a1575d318ebfac4fb70757bfb460
monpeco <monpeco@gmail.com>
2017-02-15 17:09:32 +0000

    Add sum definition and combine with sequence

--- my-until-a2.py
+++ my-until-a2.py
@@ -2,6 +2,12 @@ def my_until(top, filtro, value):
     if value==top: return []
     if filtro(value): return [value] + my_until(top, filtro, value+1)
     else: return my_until(top, filtro, value+1)
+
+def sum(p_list):
+    if len(p_list) == 0: return 0
+    return p_list[0] + sum(p_list[1:])
     
 result=my_until(10, lambda x: x%2==0, 0)
-print(result)
+print("Sequence: %s" % result)
+sum_result = sum(result)
+print("Sum: %s" % sum_result)
