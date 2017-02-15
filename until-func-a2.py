aa2eb0ad8492fcb0e78bdbb730628f450206857c
monpeco <monpeco@gmail.com>
2017-02-15 12:15:20 +0000

    Add lambda and print the output

--- until-func-a2.py
+++ until-func-a2.py
@@ -2,3 +2,8 @@ def until(n, filter_func, v):
     if v == n: return []
     if filter_func(v): return [v] + until( n, filter_func, v+1 )
     else: return until(n, filter_func, v+1)
+    
+out = until(10, lambda x: x%3==0 or x%5==0, 0)
+print(out)
+# >>> [0, 3, 5, 6, 9]   
+
