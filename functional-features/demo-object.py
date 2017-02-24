eceb7ae6c77953e30a82db1dd7e2f419a233399b
monpeco <monpeco@gmail.com>
2017-02-24 12:46:22 +0000

    Explain how function def creates an object

---
+++ functional-features/demo-object.py
@@ -0,0 +1,17 @@
+def example(a, b, **kw):
+    return a*b
+    
+variable = 2
+
+print(type(example))
+print(example)
+print(type(variable))
+print(variable)
+
+"""
+<type 'function'>
+<function example at 0x7fa138a4a578>
+<type 'int'>
+2
+
+"""
